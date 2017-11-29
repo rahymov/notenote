2c22817a02768d212272772ae9ff8b271f3ff024
Yazgylych <y.rahymov@gmail.com>
2017-11-29 09:21:12 -0500

    Generate notes controller & routes

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,31 @@
+class NotesController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def edit
+  end
+
+  def destroy
+  end
+
+  def show
+  end
+
+  def index
+  end
+
+  private
+
+  def find_note
+    
+  end
+  def note_params
+    
+  end
+end
