30bbfc4b4631e6717d9909d91460682cf8c48c97
Yazgylych <y.rahymov@gmail.com>
2017-11-30 09:18:30 -0500

    root changed if user_signed_in

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
  
-  devise_for :users
-	resources :notes
+  devise_for :users	
   get 'welcome/index'
+  resources :notes
 
+  authenticated :user do 
+  	root "notes#index", as: "authenticated_root"
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "welcome#index"
 end
